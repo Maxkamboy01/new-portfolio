af11195295a3a9af0cc64be433753b0da28cc70b
Maxkamboy <mahkamboyxusnidinov@gmail.com>
2021-09-29 21:45:52 +0300

    shit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var arg = 0;
 
-
 function increment(e) {
-    e.preventDefault()
-    arg+1;
-    document.getElementById('a').innerText = arg + "kg";
-}
-function decrement() {
-    arg--;
-    document.getElementById('a').innerText = arg + "kg";
-    if (arg <= 0) {
-        document.getElementById('a').innerText = 0;
-    }
+  e.preventDefault();
+  arg + 1;
+  document.getElementById("a").innerText = arg + "kg";
 }
 
+function decrement(e) {
+    e.preventDefault()
+  arg-1;
+  document.getElementById("a").innerText = arg + "kg";
+  if (arg <= 0) {
+    document.getElementById("a").innerText = 0;
+  }
+}
